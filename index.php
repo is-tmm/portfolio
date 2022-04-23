8ea0fc3e7e1e833e650fc5266ac58c164d699cc7
Ishikawa <tmm.ishikawa.giver@gmail.com>
2022-04-24 01:02:41 +0900

    add header部分

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>hikikomo</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic:wght@700&family=Noto+Sans+JP&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header class="l-header">
+        <div class="c-header">
+            <h1 class="c-header__site-title">ヒキコモリエンジニアのEvernote的な</h1>
+        </div>
+    </header>
+    <div class="l-main">
+
+    </div>
+    <section>
+        <p>いしかわ　ともみ</p>
+        <p>2020年 <a href="https://webukatu.com/">ウェブカツ</a>卒業生</p>
+        <p>ツイッター</p>
+        <p>GitHub</p>
+        <p>メール</p>
+    </section>
+    <section>
+        portfolio
+    </section>
+    <footer>
+        <div>
+            aaaa
+        </div>
+    </footer>
+<script src="main.js"></script>
+</body>
+</html>
