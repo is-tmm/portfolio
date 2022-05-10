9dffbbfe30e6875438439e1de9c8dd33e7fd8bfa
Ishikawa <tmm.ishikawa.giver@gmail.com>
2022-05-10 17:43:23 +0900

    modify index

--- index.php
+++ index.php
@@ -3,40 +3,77 @@
 <head>
     <meta charset="UTF-8">
     <title>hikikomo</title>
+    <link rel="icon" href="/favicon.ico" />
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
+    
 
     <!-- google font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic:wght@700&family=Noto+Sans+JP&display=swap" rel="stylesheet">
 
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/18cbab8d55.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <header class="l-header">
         <div class="c-header">
             <h1 class="c-header__site-title">ヒキコモリエンジニアのEvernote的な</h1>
-        </div>
+        </div><!-- /c-header -->
     </header>
-    <div class="l-main">
+    <main class="l-main">
+        <section class="p-profile">
+            <div class="p-profile__image">
+                <img class="c-img--cover"
+                    src="./src/image/ナマケモノ.svg"
+                    alt="ナマケモノのイラスト" />
+            </div><!-- /p-profile__image -->
+            <div class="p-profile__text">
+                <ul class="p-profile__detail">
+                    <li>石川　智美</li>
+                    <li>Ishikawa Tomomi</li>
+                    <li>2020年 ウェブカツ卒業生</li>
+                </ul><!-- /p-profile__detail -->
+                
+                <ul class="p-profile__sns">
+                    <li class="p-profile__sns--icon">
+                        <a href="">
+                            <img class="c-img--cover"
+                                src="./src/icon/Twitter-circle-blue.png"
+                                alt="ツイッターアイコンリンク" />
+                        </a>
+                    </li><!-- /p-profile__sns--icon-->
+                    <li class="p-profile__sns--icon">
+                        <a href="http://">
+                            <img class="c-img--cover"
+                                src="./src/icon/GitHub-Mark-120px-plus.png"
+                                alt="gitHubアイコンリンク" />
+                        </a>
+                    </li><!-- /p-profile__sns--icon-->
+                    <li class="p-profile__sns--icon">
+                        <a href="">
+                            <img class="c-img--cover"
+                                src="./src/icon/envelope-solid.svg"
+                                alt="メールアイコンリンク" />
+                        </a>
+                    </li><!-- /p-profile__sns--icon-->
+                </ul><!-- /p-profile__sns -->
+            </div><!-- /p-profile__text -->
+        </section><!-- /p-profile -->
+        <section>
+            <h2 class="p-boder">portfolio</h2>
 
-    </div>
-    <section>
-        <p>いしかわ　ともみ</p>
-        <p>2020年 <a href="https://webukatu.com/">ウェブカツ</a>卒業生</p>
-        <p>ツイッター</p>
-        <p>GitHub</p>
-        <p>メール</p>
-    </section>
-    <section>
-        portfolio
-    </section>
+        </section>
+    </main>
+
     <footer>
         <div>
             aaaa
         </div>
     </footer>
-<script src="main.js"></script>
+    <script src="main.js"></script>
 </body>
 </html>
